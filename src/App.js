a2cd3980275bfb8dfce1eb9a0f361742a2934a1d
subhramukherjee2398 <subhramukherjee560@gmail.com>
2022-03-06 20:26:56 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useDispatch, useSelector } from "react-redux";
+import { decNum, inCNum } from "./actions";
+import "./App.css";
+
 
 function App() {
+  const myState  =  useSelector((state)=>state.changeNum)
+  const dispatch  =  useDispatch()
   return (
     <div className="App">
-     <h2>Redux Counter</h2>
+      <h2
+        style={{
+          color: "grey",
+          fontSize: "20px",
+        }}
+      >
+        Redux Counter
+      </h2>
+      <div>
+        <button
+          style={{
+            width: "80px",
+            backgroundColor: "blue",
+            color: "white",
+            border: "2px solid white",
+            padding: "0px",
+            fontSize: "20px",
+          }}
+          onClick={()=>dispatch(inCNum())}
+        >
+          +
+        </button>
+      </div>
+      <div>{myState}</div>
+      <div>
+        <button
+          style={{
+            width: "80px",
+            backgroundColor: "red",
+            color: "white",
+            border: "2px solid white",
+            padding: "0px",
+            fontSize: "20px",
+          }}
+          onClick={()=>dispatch(decNum())}
+        >
+          -
+        </button>
+      </div>
     </div>
   );
 }
